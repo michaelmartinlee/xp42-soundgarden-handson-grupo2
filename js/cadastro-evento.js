d851e98272e9a9983b7ef18e309d3da2007e8769
Rafael Sangali <rafaelsangali@outlook.com>
2022-04-26 11:07:28 -0300

    Painel de controle finalizado, aparentemente tudo OK

--- js/cadastro-evento.js
+++ js/cadastro-evento.js
@@ -1,4 +1,4 @@
-
+// Criar Evento
 const btnSubmit = document.querySelector(".btn-primary");
 
 btnSubmit.addEventListener("click", () => cadastrarEvento());
@@ -6,11 +6,15 @@ btnSubmit.addEventListener("click", () => cadastrarEvento());
 
 function fazPost(url, corpo) {
     fetch(url, {
-        method: "POST",
+        method: 'POST',
         headers: { 'Content-type': "application/json", },
         body: JSON.stringify(corpo),
     })
-        .then(() => console.log("foi"));
+        .then(() => console.log(JSON.stringify(corpo)))
+        .then(() => alert('Evento Cadastrado com Sucesso'))
+        .then(() => window.location.href = "admin.html")
+        .catch((error) => alert('Não foi possível realizar o cadastro, tente novamente'))
+
 }
 
 
@@ -20,10 +24,11 @@ function cadastrarEvento() {
     event.preventDefault()
     const url = "https://xp41-soundgarden-api.herokuapp.com/events";
     let nome = document.querySelector("#nome").value;
-    let atracoes = document.querySelector("#atracoes").value;
+    let atracoes = document.querySelector("#atracoes").value.split(", ");
     let descricao = document.querySelector("#descricao").value;
     let data = document.querySelector("#data").value;
     let lotacao = document.querySelector("#lotacao").value;
+
 
     corpo =
     {
---
+++ js/editar-evento.js
@@ -0,0 +1,50 @@
+const formSelector = document.querySelector('#form');
+const nameSelector = document.querySelector('#nome');
+const bannerSelector = document.querySelector('#banner');
+const attractionsSelector = document.querySelector('#atracoes');
+const descriptionSelector = document.querySelector('#descricao');
+const dateSelector = document.querySelector('#data');
+const capacitySelector = document.querySelector('#lotacao');
+
+const urlObject = new URLSearchParams(window.location.search);
+
+function InputValues(data) {
+    nameSelector.value = data.name;
+    bannerSelector.value = data.poster;
+    attractionsSelector.value = data.attractions.join(', ');
+    descriptionSelector.value = data.description;
+    dateSelector.value = data.scheduled.substring(0, 16);
+    capacitySelector.value = data.number_tickets;
+}
+
+fetch('https://xp41-soundgarden-api.herokuapp.com/events/' + urlObject.get('id'),
+    { "method": "GET" }
+).then(response => response.json()).then(data => InputValues(data)).catch(error => console.error(error));
+
+
+formSelector.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const body = {
+        "name": nameSelector.value,
+        "attractions": attractionsSelector.value.split(', '),
+        "poster": bannerSelector.value,
+        "description": descriptionSelector.value,
+        "scheduled": dateSelector.value,
+        "number_tickets": capacitySelector.value
+    }
+
+    fetch('https://xp41-soundgarden-api.herokuapp.com/events/' + urlObject.get('id'),
+        {
+            "method": "PUT",
+            "headers": { "content-type": "application/json" },
+            "body": JSON.stringify(body)
+        }
+    ).then(response => console.log(response))
+        .then(() => alert("Evento editado com Sucesso"))
+        .then(() => window.location.href = "admin.html")
+        .catch(error => console.error(error));
+
+});
+
+
