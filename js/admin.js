fdcdc97b367a33c9b504c7cd8924467b20efad4c
FelixMembrive <felix.membrive@gmail.com>
2022-04-28 21:11:32 -0300

    Ajuste na listagem das reservas

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,4 @@
-async function getEvents() {  // Faz requisição na Api, para preencher o painel com os eventos
+async function getEvents() { // Faz requisição na Api, para preencher o painel com os eventos
 
     try {
         const response = await fetch('https://xp41-soundgarden-api.herokuapp.com/events')
@@ -28,16 +28,17 @@ async function getEventsToModal(id) {
         document.querySelector('#tbody-modal').innerHTML = "Não há reservas para esse evento"
         return;
     } else {
-        createListToModal(bookingList);
+        const bookingListLength = bookingList.length
+        createListToModal(bookingList, bookingListLength);
     }
 
 }
 
 
-async function createElementsFromEvents(data) {     //Função que cria os todos os eventos na pagina
+async function createElementsFromEvents(data) { //Função que cria os todos os eventos na pagina
     const tableSelector = document.querySelector('.table'); // selcionando a tabela
     const tableBodySelector = tableSelector.childNodes[3];
-    data.forEach((event, index) => {  // percorrendo todos os eventos
+    data.forEach((event, index) => { // percorrendo todos os eventos
         const trElement = document.createElement('tr');
 
         const thElement = document.createElement('th');
@@ -55,7 +56,7 @@ async function createElementsFromEvents(data) {     //Função que cria os todos
         const thirdTdElement = document.createElement('td');
         thirdTdElement.innerText = event.attractions.join(', ');
         console.log(window.screen.width)
-        if (window.screen.width <= 480) {   // para responsividade
+        if (window.screen.width <= 480) { // para responsividade
             thirdTdElement.setAttribute('style', 'display:none')
         }
 
@@ -68,18 +69,18 @@ async function createElementsFromEvents(data) {     //Função que cria os todos
         firstAnchor.setAttribute('data', event._id);
         firstAnchor.addEventListener('click', () => {
 
-            openAndCloseModal();    // Função que permite abrir e fechar o modal
-            getEventsToModal(event._id);    // Função que faz requisição para Api para receber info
+            openAndCloseModal(); // Função que permite abrir e fechar o modal
+            getEventsToModal(event._id); // Função que faz requisição para Api para receber info
         })
 
 
-        const secondAnchor = document.createElement('a');   // Botão editar evento
+        const secondAnchor = document.createElement('a'); // Botão editar evento
         secondAnchor.innerText = "editar";
         secondAnchor.classList.add('btn');
         secondAnchor.classList.add('btn-secondary');
         secondAnchor.href = 'editar-evento.html?id=' + event._id;
 
-        const thirdAnchor = document.createElement('a');    // Botão excluir evento
+        const thirdAnchor = document.createElement('a'); // Botão excluir evento
         thirdAnchor.innerText = "excluir";
         thirdAnchor.classList.add('btn');
         thirdAnchor.classList.add('btn-danger');
@@ -94,46 +95,46 @@ async function createElementsFromEvents(data) {     //Função que cria os todos
     })
 }
 
-async function openAndCloseModal() {  // Função que abre e fecha o modal
+async function openAndCloseModal() { // Função que abre e fecha o modal
     const modal = document.querySelector(".myModal");
     modal.setAttribute('style', 'display:block')
 
-    const span = document.getElementsByClassName("close")[0];   // Clicar no botão x para fechar
-    span.onclick = function () {
+    const span = document.getElementsByClassName("close")[0]; // Clicar no botão x para fechar
+    span.onclick = function() {
         modal.style.display = "none";
     }
 
-    window.onclick = (event) => {  // Clicar fora do modal, fecha ele
+    window.onclick = (event) => { // Clicar fora do modal, fecha ele
         if (event.target == modal) {
             modal.style.display = "none";
         }
     }
 }
 
-async function createListToModal(data) {    //Função que cria o modal dinâmicamente
+async function createListToModal(data, dataLength) { //Função que cria o modal dinâmicamente
     const thead = document.querySelector('#thead-modal')
     thead.setAttribute('style', 'display:deafult')
     document.querySelector('#tbody-modal').innerHTML = null
 
-    const dataObject = data[0]
+    for (let index = 0; index < dataLength; index++) {
+        const dataObject = data[index]
+        const trElement = document.createElement('tr');
 
-    const trElement = document.createElement('tr');
+        const thElement = document.createElement('th');
+        thElement.setAttribute('scope', 'row')
 
-    const thElement = document.createElement('th');
-    thElement.setAttribute('scope', 'row')
+        const nameTdElement = document.createElement('td');
+        nameTdElement.innerText = dataObject.owner_name;
 
-    const nameTdElement = document.createElement('td');
-    nameTdElement.innerText = dataObject.owner_name;
+        const emailTdElement = document.createElement('td');
+        emailTdElement.innerText = dataObject.owner_email;
 
-    const emailTdElement = document.createElement('td');
-    emailTdElement.innerText = dataObject.owner_email;
+        const ticketsTdElement = document.createElement('td');
+        ticketsTdElement.innerText = dataObject.number_tickets;
 
-    const ticketsTdElement = document.createElement('td');
-    ticketsTdElement.innerText = dataObject.number_tickets;
+        const tableBodyModalSelector = document.querySelector('#tbody-modal')
+        tableBodyModalSelector.appendChild(trElement);
+        trElement.append(thElement, nameTdElement, emailTdElement, ticketsTdElement);
 
-    const tableBodyModalSelector = document.querySelector('#tbody-modal')
-    tableBodyModalSelector.appendChild(trElement);
-    trElement.append(thElement, nameTdElement, emailTdElement, ticketsTdElement);
-
-
+    }
 }
